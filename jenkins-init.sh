45e921e7a975dc7c86a5af5e1aeafdc4c451b248
ErikPopoo <122523699+ErikPopoo@users.noreply.github.com>
2023-02-22 07:43:14 +0800

    Update jenkins-init.sh

--- jenkins-init.sh
+++ jenkins-init.sh
@@ -3,6 +3,14 @@
 systemctl enable docker
 systemctl start docker
 
-cat <<EOF > /root/hello.txt
-Hello World!
+cat <<EOF > /root/eclipse-temurin-with-maven/Dockerfile
+FROM eclipse-temurin:18-jdk-alpine
+
+RUN wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.zip && unzip apache-maven-3.8.7-bin.zip -d /usr/local/
+
+ENV MAVEN_HOME=/usr/local/apache-maven-3.8.7
+ENV PATH=$PATH:$MAVEN_HOME/bin
 EOF
+
+cd /root/eclipse-temurin-with-maven
+docker build -t eclipse-temurin:18-jdk-alpine-maven-3.8 .
