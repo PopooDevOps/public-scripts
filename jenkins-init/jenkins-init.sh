9a9e501f29d48f2ab4110da79ffe3e848800d3bb
ErikPopoo <xz@popoo.io>
2023-02-22 06:59:42 +0000

    Modify jenkins init script.

--- jenkins-init/jenkins-init.sh
+++ jenkins-init/jenkins-init.sh
@@ -4,15 +4,7 @@ systemctl enable docker
 systemctl start docker
 
 mkdir -p /root/eclipse-temurin-with-maven
-
-cat <<EOF > /root/eclipse-temurin-with-maven/Dockerfile
-FROM eclipse-temurin:18-jdk-alpine
-
-RUN wget https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.zip && unzip apache-maven-3.8.7-bin.zip -d /usr/local/
-
-ENV MAVEN_HOME=/usr/local/apache-maven-3.8.7
-ENV PATH=$PATH:$MAVEN_HOME/bin
-EOF
-
 cd /root/eclipse-temurin-with-maven
+rm -f Dockerfile
+wget https://raw.githubusercontent.com/PopooDevOps/public-scripts/main/jenkins-init/eclipse-temurin-with-maven/Dockerfile
 docker build -t eclipse-temurin:18-jdk-alpine-maven-3.8 --network host .
