c65aac9ca4723ac703bf41a25b5f332e4aff14b7
LynnZhang10 <zn@popoo.io>
2024-01-03 16:22:01 +0800

    adjust address to github

--- jenkins-init/jenkins-init.sh
+++ jenkins-init/jenkins-init.sh
@@ -6,5 +6,5 @@ systemctl start docker
 mkdir -p /root/eclipse-temurin-with-maven
 cd /root/eclipse-temurin-with-maven
 rm -f Dockerfile
-wget http://git.popoo.foundation/devops/public-scripts/-/raw/main/jenkins-init/eclipse-temurin-with-maven/Dockerfile
+wget https://github.com/PopooDevOps/public-scripts/blob/main/jenkins-init/eclipse-temurin-with-maven/Dockerfile
 docker build -t eclipse-temurin:18-jdk-alpine-maven-3.8 --network host .
