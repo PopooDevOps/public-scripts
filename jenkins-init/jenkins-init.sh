906ef06316773bb27520a7cda3deb9fac96e86c5
Himanshu B <hb@popoo.io>
2024-01-04 08:51:26 +0530

    chore: update dockerfile url

--- jenkins-init/jenkins-init.sh
+++ jenkins-init/jenkins-init.sh
@@ -6,5 +6,5 @@ systemctl start docker
 mkdir -p /root/eclipse-temurin-with-maven
 cd /root/eclipse-temurin-with-maven
 rm -f Dockerfile
-wget https://github.com/PopooDevOps/public-scripts/blob/main/jenkins-init/eclipse-temurin-with-maven/Dockerfile
+wget https://raw.githubusercontent.com/PopooDevOps/public-scripts/main/jenkins-init/eclipse-temurin-with-maven/Dockerfile
 docker build -t eclipse-temurin:18-jdk-alpine-maven-3.8 --network host .
